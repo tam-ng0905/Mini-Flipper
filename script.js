acd4aa304378db42785c7469bdf4cf51721993ac
Glitch (hello-webpage) <none>
2020-06-11 21:56:06 +0000

    ✌️🛋 Checkpoint ./index.html:8656122/923 ./script.js:8656122/355

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+const colors = ["green","red","rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+
+btn.addEventListener('click', function(){
+  console.log(document.body);
+})
