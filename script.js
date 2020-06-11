2a7de64e551dea914b5abd093f8f4dd2fa671f72
Glitch (hello-webpage) <none>
2020-06-11 22:07:21 +0000

    🚆🚂 Checkpoint ./script.js:8656122/418 ./hex.js:8656122/466

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ const color = document.querySelector(".color");
 
 
 btn.addEventListener('click', function(){
-  console.log(document.body);
+  //get Random number between 0 - 3 colors[3]
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 })
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * color.length);
+}
